1c02b918efc0ac263373ff6dffa54e9a06a6e123
yuruiqin-gif <110699539+yuruiqin-gif@users.noreply.github.com>
2024-02-15 15:19:07 -0600

    Completed my Github VS Code setup
    finished

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  circle(mouseX, mouseY, 20);
+}
+ 
