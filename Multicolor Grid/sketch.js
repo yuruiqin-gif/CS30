e5ff504332406c14c1558727a9c88d3ad9a05409
yuruiqin-gif <110699539+yuruiqin-gif@users.noreply.github.com>
2024-03-12 15:01:39 -0600

    Fixing the mouse function to change the grid

--- Multicolor Grid/sketch.js
+++ Multicolor Grid/sketch.js
@@ -1,46 +1,54 @@
 // Multicolour Grid
 // Yurui Qin
 // May 5, 2025
-//
+//To draw a coloured grid affected by mouse input
 
-let squareSize = 25;
+let squareSize = 25; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop();
+  grid();
+  document.addEventListener("contextmenu", event => event.preventDefault())
 }
 
 function draw() {
-  grid();
+
 }
+function fitter(){
+  if (squareSize*squareSize*width < width){
+    clear();
+  }
+  
+}
+
 
 function grid(){
   for (let x = 0; x < width; x += squareSize) {
     for (let y = 0; y < height; y += squareSize) {
       stroke(0);
       square(x,y,squareSize);
-      fill(random(250),random(250),random(250));
+      fill(random(250),random(0),random(250));
     }
   }
 }
 
 
 function mousePressed() {
+  grid();
   if (mouseButton === RIGHT) {
-    squareSize += 50;
-
-    if (squareSize >= 100){
-      squareSize = 100;
-    }
+    squareSize = squareSize + 10;
   }
+  if (squareSize >= 100){
+    squareSize = 100;
+  }
+
   if (mouseButton === LEFT) {
     squareSize -= 5;
-
-    if (squareSize <= 5){
-      squareSize = 5;
-    }
-    
   }
+  if (squareSize <= 5){
+    squareSize = 5;
+  }
+  
 }
 
 function keyPressed(){
