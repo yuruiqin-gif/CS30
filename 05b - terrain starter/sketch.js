03761c150a0e4a0a23dc55d7d5faeb24a7d0b65e
yuruiqin-gif <110699539+yuruiqin-gif@users.noreply.github.com>
2024-03-12 15:18:21 -0600

    Fixing the for loop to draw randomized mountain heights

--- 05b - terrain starter/sketch.js
+++ 05b - terrain starter/sketch.js
@@ -3,36 +3,44 @@
 // March 11, 2024
 // Precedurally Generated 2D Terrain
 
-let rectWidth = 1;
+let rectWidth = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   rectMode(CENTER); //CHANGE THIS
-  drawRectangles();
 }
 
 function drawRectangles(){
   //using a single loop, generate a bunch of side-to-side
   //rectangles of varying height (pattern, random, noise)
   let rectHeight;
-  let rectSize;
+  // let rectSize;
   fill(0);
   for(let x = 0; x < width; x += rectWidth){
-  //   //Option 1 - pattern
-  //   // rectHeight = x;
+    //Option 1 - pattern
+    rectHeight = x;
 
-  //   //Option 2 - random()
+    //Option 2 - random()
     rectHeight = random(0, height*0.8);
-    rectY = 100 * noise(0.005*frameCount);
-    rectSize = 100 * noise(0.005*frameCount);
-    rect(rectSize, rectY, rectWidth, rectHeight);
+    rect(x, height/2, rectWidth, rectHeight);
+
+
+
+    // rectHeight = 100 * noise(0.1*frameCount);
+    // rectY = 100 * noise(0.1*frameCount);
+    // rectSize = 100 * noise(0.1*frameCount);
+    // rect(x, height/2, rectWidth, rectHeight);
 
     // rectHeight = random(0, height*0.8);
     // rectY = 100 * noise(100*frameCount);
     // rectSize = 100 * noise(100*frameCount);
     // rect(x, 200, rectWidth, rectHeight);
 
+
+
+    /////
+    
   }
   
 }
