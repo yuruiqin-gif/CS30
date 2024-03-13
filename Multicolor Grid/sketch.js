4713ba6f225a23571fd102d2e8834b96c1af3b9a
Yurui <yuruiqin188@gmail.com>
2024-03-12 23:08:50 -0600

    Comments added

--- Multicolor Grid/sketch.js
+++ Multicolor Grid/sketch.js
@@ -1,49 +1,49 @@
 // Multicolour Grid
 // Yurui Qin
-// May 5, 2025
+// March 12, 2025
 //To draw a coloured grid affected by mouse input
 
-let squareSize = 25; 
+let squareSize = 25; //Starting size of the square
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid();
-  document.addEventListener("contextmenu", event => event.preventDefault())
+  document.addEventListener("contextmenu", event => event.preventDefault()) //gets rid of pop up when mouse right clicks
 }
 
 function draw() {
 
 }
 
-function grid(){
-  for (let x = 0; x < width; x += squareSize) {
+function grid(){ 
+  for (let x = 0; x < width; x += squareSize) { //draws a grid that fits in frame
     for (let y = 0; y < height; y += squareSize) {
       stroke(0);
-      square(x,y,squareSize);
-      fill(random(250),random(0),random(250));
+      square(x,y,squareSize); //draws a square
+      fill(random(250),random(50),random(100)); //semi-randomly fills the square with different colour
     }
   }
 }
 
 
 function mousePressed() {
-  if (mouseButton === RIGHT) {
+  if (mouseButton === RIGHT) { //Increase size of the squares if mouse right-clicked
     squareSize += 5;
   }
-  if (squareSize >= 500){
+  if (squareSize >= 500){ //To avoid square dissapearing, if the square is too large it will not increase
     squareSize = 500;
   }
 
-  if (mouseButton === LEFT) {
+  if (mouseButton === LEFT) { //Decrease size of the squares if mouse left-clicked
     squareSize -= 1;
   }
-  if (squareSize <= 10){
+  if (squareSize <= 10){ //To avoid program crashing, if the squares are too small it will not decrease any more
     squareSize = 510;
   }
-  clear();
+  clear(); //Draws the current grid and removes previous grids
   grid();
 }
 
 function keyPressed(){
-    grid();
+    grid(); //If a key is pressed then change the colour of the squares
 }
