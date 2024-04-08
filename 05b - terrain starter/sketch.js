aa27ee4e04d3aae93ba6955c1ce2e89202869e1b
Yurui <yuruiqin188@gmail.com>
2024-04-08 02:13:11 -0600

    Added comments and organized code
    added the final touches

--- 05b - terrain starter/sketch.js
+++ 05b - terrain starter/sketch.js
@@ -1,84 +1,87 @@
-// Terrain Generation Starter
+// Terrain Generation
 // Yurui Qin
-// March 13, 2024
-// Precedurally Generated 2D Terrain
+// CS30
+// April.8, 2024
+// Gernerate a infinite mountain range that finds the highest peak and calculates the mountain height average
 
 let PerlinStart = 0;
-let Increment= 0.01;
+let Increment= 0.01; //variable to increment noise value per rectangle
 
-///////////
-let rectWidth = 1;
-let mountLine = 0; //time
+let rectWidth = 1; //width of the mountain's rectangles
+let mountTime = 0; //time for perlin noise
 
+//variables to find the average hieght
 let heightAvg = 0;
 let MountTotal = 0;
 let MountNum = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-  rectMode(CENTER); //CHANGE THIS
-  drawRectangles();
+  rectMode(CENTER);
 }
 
-function drawRectangles(){
+function generateTerrain(){
   background(255);
   stroke(0,0,0);
-  mountLine = PerlinStart;
 
-  let tallPointX = 0;
+  mountTime = PerlinStart; //Incrementing the time value to pan
+
+  let tallPointX = 0; //Initializing the coordinates for the highest peak
   let tallPointY = 0;
 
-  MountTotal = 0;
+  MountTotal = 0; //Initializing the variables to find average height
   MountNum = 0; 
 
   for(let x = 0; x < width; x++){
+  
+    y = map(noise(mountTime),0,1,0,height); //variable for height of the mountains
 
-    y = map(noise(mountLine),0,1,0,height);
     //finding the highest peak
     if(y > tallPointY){
       tallPointY = y;
       tallPointX = x;
     }
-    MountTotal += y;
-    MountNum++;
+
+    MountTotal += y; //adding to the sum of all heights
+    MountNum++; //adding to the number of mountains
 
     rect(x, height, rectWidth, y*2);
-    mountLine += Increment;
+
+    mountTime += Increment; //Changing the value of the noise to draw rectangle close to the last
   }
-  heightAvg = MountTotal/MountNum;
+
+  heightAvg = MountTotal/MountNum; //finding the average height
 
   drawFlag(tallPointX, height-tallPointY);
   
-  PerlinStart += 0.01;
+  PerlinStart += 0.05; //Incrementing the time value to pan
 
+  //drawing a line to repersent the average height
   stroke(255,0,0);
   line(0,heightAvg,windowWidth,heightAvg);
 
   fill(0,0,0);
 }
 
-function draw() {
-  drawRectangles();
-  // drawFlag(highestPoint,rectWidth);
-  // drawFlag(100,50);
-}
-
-function drawFlag(x,y){
+function drawFlag(x,y){ //function to draw the flag on the highest peak
   line(x,y,x,y-10);
   fill(255,0,0);
   triangle(x,y-10,x,y-20,x+20,y-10);
 }
 
 function keyPressed(){
-  if (keyCode === LEFT_ARROW){
+  if (keyCode === LEFT_ARROW){ //pressing left arrow narrows mountains
     rectWidth -= 1;
   }
-  if (rectWidth <= 1){
+  if (rectWidth <= 1){ //parameter to avoid crashing the program
     rectWidth = 1;
   }
 
-  if (keyCode === RIGHT_ARROW){
+  if (keyCode === RIGHT_ARROW){ //pressing right arrow widens mountains
     rectWidth += 1;
   }
+}
+
+function draw() {
+  generateTerrain();
 }
