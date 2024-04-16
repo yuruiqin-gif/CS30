d76fb771c024c510ba8c93cb1b00ffe137967a82
Yurui <yuruiqin188@gmail.com>
2024-04-16 09:08:22 -0600

    added comments
    comments added

--- Art Replication/sketch.js
+++ Art Replication/sketch.js
@@ -1,37 +1,42 @@
 // Art Replication Warm-up
 // Yurui Qin
+// CS30
 // April 15, 2024
-//
 // Replicating 1960s artwork “Computer Composition With Lines”
 
-rectList = [];
+rectList = []; //list for rectangle information
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  randomRect(800);
+  randomRect(400); //generates rectangle information for the list 400 times
 }
 
 function draw() {
   background(220);
-
-  // circle(width/2, height/2,500);
-
-  for (let i = 0; i < rectList.length; i+=10) {
-    rect(rectList[i][0],rectList[i][1],rectList[i][2],rectList[i][3],);
+  //draws the rectangles
+  for (let i = 0; i < rectList.length; i+=1) {
+    fill(0);
+    rect(rectList[i][0],rectList[i][1],rectList[i][2],rectList[i][3],); 
   }
 }
 
 function randomRect(n){
 
   for (let i = 0; i < n; i++){ 
-    let randomAngle = random(0,TWO_PI);
-    let equalDistribution = 10 * sqrt(random(0,100));
 
+    //picks a random point from center of circle and angle
+    let randomAngle = random(0,TWO_PI);
+    let equalDistribution = 10*sqrt(random(0, 200)); //square root function ensures equal distribution from circle center
+
+    //random x and y coordinates
     let x = width/2 + equalDistribution * cos(randomAngle);
     let y = height/2 + equalDistribution * sin(randomAngle);
+
+    //random width and height information for rectangles
     let w = random(1,10);
     let h = random(1,10);
-    rectList.push([x,y,w,h]);
+
+    rectList.push([x,y,w,h]); //add new rectangle information to the list
   }
   // fill(0);
   // rect(random((width/2)-250,(width/2)+250),random((height/2)-250,(height/2)+250),random(0, 15),random(0,15));
