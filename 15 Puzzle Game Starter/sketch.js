ece106f9b87e244bb57c651123f2be2f5e0c8f2c
yuruiqin-gif <110699539+yuruiqin-gif@users.noreply.github.com>
2024-04-24 14:52:36 -0600

    Completed Demo
    Puzzle game demo completed

--- 15 Puzzle Game Starter/sketch.js
+++ 15 Puzzle Game Starter/sketch.js
@@ -8,11 +8,12 @@
 //  [30, 40, 50,  60,  70],
 //  [255,255,255,255, 255]
 // ];
-let grid = 
-[[0,255, 0, 255, 200],
- [255,  0,   0,   255,   0],
- [255,0,255,0,255],
- [255,0,255,0, 255]
+
+let grid =
+[ [0,    255,    0,  255,   0],
+  [0,      0,  255,    0,   0],
+  [0,    255,  255,    0,  255],
+  [255,  255,    0,  255,  255]
 ];
 
 let squareSize = 50;
@@ -25,35 +26,48 @@ function setup() {
 }
 
 function draw() {
-  row = getCurrentX; print(ro)
+  col = getCurrentX();
+  row = getCurrentY();  print(col, row);
   background(220);
   drawGrid();
 }
 
-function getCurrenty(){
-  //determine the current si fiii
-  let 
-  let constrainX = constrain(mouseX,0,width-1);
-  return int(constrainX.squreSize);k
+function mousePressed(){
+  //always: flip the "current" title
+  flip(col, row);
+
+  //depends a bit on position: flip 4 neighbours
+  if(row < 3) flip(col+1, row); //DOWN
+}
+
+function flip(x,y){
+  if(grid[y][x]===0) grid[y][x]=255;
+  else grid[y][x] = 0;
+}
+
+function getCurrentY(){
+  //determine current row of mouse, and return
+  let constrainY = constrain(mouseY, 0, height-1);
+  return int(constrainY/squareSize);
 }
 
 function getCurrentX(){
-  //determine the current sikumn 
-  let constrainX = constrain(mouseX,0,width-1);
-  return int(constrainX.squreSize);
+  //determine the current column of the mouse, and return
+  let constrainX = constrain(mouseX, 0, width-1);
+  return int(constrainX/squareSize);
 }
 
 function drawGrid(){
-  //Read data from our 2D Array (grid), and use the
-  //numbers there to set the color for squares which are
-  //arranged in a grid fashion.
+  // Read data from our 2D Array (grid), and use the 
+  // numbers there to set the color for squares which are
+  // arranged in a grid fashion.
   for(let y = 0; y<NUM_ROWS; y++){
     for(let x = 0; x<NUM_COLS; x++){
       let fillValue = grid[y][x];
       fill(fillValue);
-      //            x: 0, 1, 2,  3,  4
-      //squareSize*x:  0,50,100,150,200
-      square(x*squareSize,y*squareSize, squareSize);
-    }
+      //             x:   0 ,   1,     2,    3,     4  
+      //squareSize*x:     0     50    100    150    200
+      square(x*squareSize, y*squareSize, squareSize);
+    }  
   }
 }
